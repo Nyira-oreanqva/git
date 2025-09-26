c9d704249743dda5d00707674a41e74d38ca1304
Nyira-oreanqva <oreanqva13@gmail.com>
2025-09-26 01:51:56 -0400

    modifie

---
+++ entrep.h
@@ -0,0 +1,41 @@
+/*------------------------------------------------------------------------*/
+/* FCHIER:                          entrep.h                              */
+/*AUTEUR:                           PIERRE-LOUIS Alven Bernadin           */    
+/*DATE DE CREATIION:                26/09/2025                            */  
+/*DATE DE MODIFICATION:             26/09/2025                            */ 
+/*DESCRIPTION:                      fichier contenant les nouveaux types de
+                                    données, les prototypes, les structures 
+                                    de données et les constantes          */    
+/*------------------------------------------------------------------------*/ 
+
+#ifndef ENTREP_H
+#define ENTREP_H
+
+//structure usine
+typedef struct{
+    int Id_usine;
+    int Id_com;
+    char Usi_nom[50];
+    char Usin_desc[200];
+}Usine;
+
+//structure produit
+typedef struct{
+    int Id_prod;
+    int Id_usine;
+    char Pro_nom[50];
+    char pro_desc[200];
+    float pro_prix;
+    int pro_qte;
+}Produit;
+
+//structure Historique production
+typedef struct{
+    int Id_lot;
+    int id_prod;
+    char Dat_prod[11];
+    char Dat_exp[11];
+    int Lot_qte;
+}Hist_production;
+
+#endif // ENTREP_H
---
+++ supplementaire.c
@@ -0,0 +1,14 @@
+
+/*------------------------------------------------------------------------*/
+/* FCHIER:                          suplementaire.c                       */
+/*AUTEUR:                           PIERRE-LOUIS Alven Bernadin           */
+/*DATE DE CREATIION:                26/09/2025                            */
+/*DATE DE MODIFICATION:             26/09/2025                            */
+/*DESCRIPTION:                      fichier contenant des fonctions 
+                                    supplémentaires necessaire.           */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "entrep.h"
+
+//fonction pour generer un ID de 10 chiffres de maniere aleatoire
